975fb479ee8da6f8c413fcaa9ea4feb8ead11203
Potcharapol Nokyoo <potcharapol.nok@gmail.com>
2024-05-20 18:19:38 +0700

    [feat]: completed

--- src/services/user/user.resolver.ts
+++ src/services/user/user.resolver.ts
@@ -1,5 +1,5 @@
 import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
-import { RegisterUserInput } from './user.input';
+import { LoginUserInput, RegisterUserInput } from './user.input';
 import { UserService } from './user.service';
 import { User } from './user.schema';
 
@@ -10,5 +10,10 @@ export class UserResolver {
   @Mutation(() => User)
   async registerUser(@Args('input') input: RegisterUserInput) {
     return this.userService.registerUser(input);
+  }
+
+  @Mutation(() => User)
+  async loginUser(@Args('input') input: LoginUserInput) {
+    return this.userService.loginUser(input);
   }
 }
